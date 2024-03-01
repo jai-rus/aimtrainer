6a6ff40ca2538476db9a40c30602811e984fba77
Jairus <117532457+jai-rus@users.noreply.github.com>
2024-03-01 00:18:37 -0800

    Update aimtrainer.py

--- aimtrainer/aimtrainer.py
+++ aimtrainer/aimtrainer.py
@@ -134,7 +134,7 @@ else:
         #Twitter Poster
         oauth = OAuth()
         api = tweepy.API(oauth)
-        api.update_status("i have a secret supply of feet pictures of ashley tan")
+        api.update_status("i smell bad)
     
     ending = font.render(end_text, True, (0, 0, 0))
     win.blit(ending, (200, 425))
